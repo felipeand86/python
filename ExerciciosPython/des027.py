4fbcb11a633349959110c2962f6f83dd7dae1964
Felipe André <felipe.tecpro26@gmail.com>
2025-10-22 12:03:20 -0300

    separando strings

---
+++ ExerciciosPython/des027.py
@@ -0,0 +1,5 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Analisando seu nome..')
+print('Seu nome tem {} letras'.format(len(nome)))
+print('Seu primeiro nome é {}'.format(nome.split()[0]))
+print('Seu último nome é {} '.format(nome.split()[-1]))
