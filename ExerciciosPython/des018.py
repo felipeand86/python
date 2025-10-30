42becf5135d817cd97aba2ad726db43e7f5878cc
Felipe André <felipe.tecpro26@gmail.com>
2025-10-30 15:20:43 -0300

    cores

--- ExerciciosPython/des018.py
+++ ExerciciosPython/des018.py
@@ -12,10 +12,10 @@ print('O angulo de {} tem a tangente de {:.2f}'.format(angulo, tangente))
 from math import tan, sin, cos, radians
 angulo = float(input('Digite um angulo: '))
 seno = sin(radians(angulo))
-print('O angulo de {} tem o seno de {:.2f}'.format(angulo, seno))
+print('O angulo de {} tem o \33[34mseno de {:.2f}\33[m'.format(angulo, seno))
 cosseno = cos(radians(angulo))
-print('O angulo de {} tem o cosseno de {:.2f}'.format(angulo, cosseno))
+print('O angulo de {} tem o \33[33mcosseno de {:.2f}\33[m'.format(angulo, cosseno))
 tangente = tan(radians(angulo))
-print('O angulo de {} temm a tangente de {:.2f}'.format(angulo, tangente))
+print('O angulo de {} tem a \33[31mtangente de {:.2f}\33[m'.format(angulo, tangente))
 
 
