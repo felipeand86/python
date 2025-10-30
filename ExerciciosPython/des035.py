04b832a5a7b4ce77dc09b8e0d6d37e846aae7a65
Felipe André <felipe.tecpro26@gmail.com>
2025-10-30 11:16:09 -0300

    analisa triangulo

---
+++ ExerciciosPython/des035.py
@@ -0,0 +1,10 @@
+print('-=' * 20)
+print('Analisador de Triângulos')
+print('-=' * 20)
+r1 = float(input('Digite o primeiro valor: '))
+r2 = float(input('Digite o segundo valor: '))
+r3 = float(input('Digite o terceiro valor: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os valores digitador PODEM formar um triângulo!')
+else:
+    print('Os valores digitados NÃO PODEM formar um triângulo!')
