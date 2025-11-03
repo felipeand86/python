630d0a70b2be26053e84787cd379f3029641399b
Felipe André <felipe.tecpro26@gmail.com>
2025-11-03 19:07:23 -0300

    compara numero

--- ExerciciosPython/des038.py
+++ ExerciciosPython/des038.py
@@ -1,9 +1,9 @@
-v1 = int(input('Digite um valor: '))
-v2 = int(input('Digite outro valor: '))
-maior = v1 or v2
+v1 = int(input('Primeiro numero: '))
+v2 = int(input('Segundo numero: '))
 if v1 > v2:
-    print('{} é maior que {}'.format(maior, v1))
-if v2 > v1:
-    print('{} é maior que {}'.format(maior, v2))
-elif v1 == v2:
-    print('Não existe valor maior, os dois são iguais {} e {}'.format(v1, v2))
+    print('O primeiro numero é maior!')
+elif v2 > v1:
+    print('O segundo numero é maior!')
+else:
+    print('Os dois numeros são iguais!')
+
