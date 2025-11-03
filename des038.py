02226d799a5d95e589515546ea2b8fecf777cb45
Felipe André <felipe.tecpro26@gmail.com>
2025-11-03 19:10:13 -0300

    compara numero2

---
+++ des038.py
@@ -0,0 +1,9 @@
+n1 = int(input('Primeiro numero: '))
+n2 = int(input('Segundo numero: '))
+if n1 > n2:
+    print('O primeiro numero é maior!')
+elif n2 > n1:
+    print('O segundo numero é maior!')
+else:
+    print('Não existe numero maior, os dois são iguais!')
+    
