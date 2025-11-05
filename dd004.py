8d6942e8eec10ae3b3052caa5676eff6ad335b9f
Felipe André <felipe.tecpro26@gmail.com>
2025-11-05 18:33:35 -0300

    positivo e negativo

---
+++ dd004.py
@@ -0,0 +1,8 @@
+num = int(input('Escolha um número inteiro: '))
+if num == 0:
+    print('O numero digitado é zero(0)')
+elif num < 0:
+    print('Esse é um número negativo!')
+else:
+    print('Esse é um número positivo!')
+    
