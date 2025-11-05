65a95d7c5aa5200577ec4f437294961589c930dc
Felipe André <felipe.tecpro26@gmail.com>
2025-11-05 09:37:07 -0300

    Alistamento vsc

--- des039.py
+++ des039.py
@@ -1,17 +1,19 @@
 from datetime import date
 atual = date.today().year
-nasc = int(input('Ano de nascimento: '))
+nasc = int(input('Qual o seu ano de nascimento? '))
 idade = atual - nasc
-print('Quem nasceu no ano de {} tem {} anos no ano de {}.'.format(nasc, idade, atual))
+print('Quem nasceu no ano de {} tem {} anos de idade no ano de {}.'.format(nasc, idade, atual))
 if idade == 18:
     print('Você deve se alistar imediatamente!')
 elif idade < 18:
     saldo = 18 - idade
     print('Ainda faltam {} anos para o seu alistamento!'.format(saldo))
     ano = atual + saldo
-    print('Seu alistamento vai acontecer no ano de {}.'.format(ano))
+    print('Seu alistamento será em {}'.format(ano))
 elif idade > 18:
     saldo = idade - 18
-    print('O seu alistamento aconteceu há {} anos.'.format(saldo))
-    ano = atual - saldo 
-    print('Você deveria ter se alistado no ano de {}.'.format(ano))
+    print('Seu alistamento já aconteceu há {} anos'.format(saldo))
+    ano = atual - saldo
+    print('Seu ano de alistamento foi em {}'.format(ano))
+
+
