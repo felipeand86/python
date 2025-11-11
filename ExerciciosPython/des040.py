a971e85934885aa9365c6ee262bfc2a1f511e261
Felipe André <felipe.tecpro26@gmail.com>
2025-11-10 21:33:16 -0300

    media pc

--- ExerciciosPython/des040.py
+++ ExerciciosPython/des040.py
@@ -1,10 +1,11 @@
-n1 = float(input('Primeira nota do aluno: '))
-n2 = float(input('Segunda nota do aluno: '))
-media = (n1 + n2) / 2
-if media >= 7:
-    print('Parabéns, aprovado!')
-elif media >= 5 and media < 7:
-    print('Você deverá fazer recuperação!')
+n1 = float(input('Primeira nota: '))
+n2 = float(input('Segunda nota: '))
+m = (n1 + n2) / 2
+print(f'A média entre as notas {n1:.1f} e {n2:.1f} é {m:.1f}')
+if m >= 7:
+    print('Aprovado!')
+elif m >= 5:
+    print('Recuperação!')
 else:
     print('Reprovado!')
-print('Sua média foi de {}'.format(media))
+
