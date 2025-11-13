d02e78abc0ef500730eb27e4919626af4fddf3bd
Felipe André <felipe.tecpro26@gmail.com>
2025-11-13 19:16:25 -0300

    contagem regressiva vc

---
+++ des046.py
@@ -0,0 +1,9 @@
+from time import sleep
+print('-=' * 20)
+print('Contagem regressiva para o ano novo!')
+print('-=' * 20)
+sleep(3)
+for c in range(10, 0, -1):
+    print(c)
+    sleep(1)            
+print('Feliz ano Novo!!!') 
