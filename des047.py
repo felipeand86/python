c60bafe62eed8918ac8afa0f1efd686fee4971db
Felipe André <felipe.tecpro26@gmail.com>
2025-11-14 13:15:57 -0300

    tabuada c/s perso vc

---
+++ des047.py
@@ -0,0 +1,6 @@
+for c in range(0, 50 + 1):
+    if c %2 == 0:
+        print(c)
+print('Fim')        
+
+    
--- des049.py
+++ des049.py
@@ -1,6 +1,15 @@
+num = int(input('Digite um numero:'))
+for c in range(1, 11):
+    result = num * c
+    print(f'{num} x {c} = {result}')
+    
+
+
+'''
 num = int(input('Digite um número: '))
 for c in range(1, 11):
     resultado = num * c
     if resultado %2 == 0: #Tabuada personalizada só vai mostrar pares
         print(f'{num} x {c} = {resultado}')
 print('Fim do programa!')        
+'''
