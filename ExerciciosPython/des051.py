37542010e01329b5d4dbf23e17302f88574b3301
Felipe André <felipe.tecpro26@gmail.com>
2025-11-18 10:07:20 -0300

    progressao aritimetica pc

---
+++ ExerciciosPython/des051.py
@@ -0,0 +1,6 @@
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+dec = primeiro + (10 - 1) * razao
+for c in range(primeiro, dec + razao, razao):
+    print(f'{c}', end='-> ')
+print('Fim')
