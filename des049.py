22975d1a3b68fc110fd9d0245c4ba763a4d1fe16
Felipe André <felipe.tecpro26@gmail.com>
2025-11-18 08:49:11 -0300

    ajuste tabuada

--- des049.py
+++ des049.py
@@ -1,15 +1,15 @@
 num = int(input('Digite um numero:'))
 for c in range(1, 11):
-    result = num * c
-    print(f'{num} x {c} = {result}')
+    print(f'{num} x {c} = {num*c}')
+print('Fim')    
+
     
 
-
+#tabuada personalizada
 '''
 num = int(input('Digite um número: '))
 for c in range(1, 11):
-    resultado = num * c
     if resultado %2 == 0: #Tabuada personalizada só vai mostrar pares
-        print(f'{num} x {c} = {resultado}')
+        print(f'{num} x {c} = {num * c}')
 print('Fim do programa!')        
 '''
