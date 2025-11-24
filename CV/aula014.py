35a9db694a1129baa1e3d3a7de01568b2cdbc3d6
Felipe André <felipe.tecpro26@gmail.com>
2025-11-24 11:27:58 -0300

    aula014 pc

---
+++ CV/aula014.py
@@ -0,0 +1,12 @@
+
+#exemplo de uso do while com a condição zero encerra o laço
+n = 1
+par = impar = 0
+while n != 0:
+    n = int(input('Digite um numero: '))
+    if n != 0:
+        if n % 2 == 0:
+            par += 1
+        else:
+            impar += 1
+print(f'Você digitou {par} numeros pares e {impar} numeros impares')
