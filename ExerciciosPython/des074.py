7fbe9ca9b9b3fd9048e14c0f8fb8ba0f26e00cee
Felipe André <felipe.tecpro26@gmail.com>
2025-12-18 11:29:11 -0300

    tuplas maior e menor pc

--- ExerciciosPython/des074.py
+++ ExerciciosPython/des074.py
@@ -1,10 +1,8 @@
 from random import randint
-maior = menor = 0
-num = randint(0, 5)
-print(f'Os valores sorteados foram {num},{num},{num},{num}')
-if num > maior:
-    num = maior
-else:
-    num = menor
-print(f'O maior valor foi {maior}')
-print(f'O menor valor foi {menor}')
+num = (randint(0, 10), randint(0, 10), randint(0, 10),
+       randint(0, 10), randint(0, 10))
+print('Os valores sorteados foram: ', end='')
+for n in num:
+    print(f' {n}', end='')
+print(f'\nO maior valor sorteado foi: {max(num)}')
+print(f'O menor valor sorteado foi: {min(num)}')
