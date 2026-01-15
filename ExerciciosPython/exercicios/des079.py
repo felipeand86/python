a4e9467a27cc5d60b57a8eb7462b15bcb9985b13
Felipe André <felipe.tecpro26@gmail.com>
2026-01-15 12:42:09 -0300

    valores unicos em uma lista

---
+++ ExerciciosPython/exercicios/des079.py
@@ -0,0 +1,13 @@
+num = []
+while True:
+    n = int(input('Digite um numero: '))
+    if n not in num:
+        num.append(n)
+        print('Numero adicionado!')
+    else:
+        print('Não adiciono numero repetido!')
+    resp = str(input('Quer continuar? [S/N]'))
+    if resp in 'Nn':
+        break
+num.sort()
+print(f'Os numeros foram: {num}')
