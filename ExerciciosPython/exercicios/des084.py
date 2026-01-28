c87b06342944f399a69f5b630a04e848ff4de307
Felipe André <felipe.tecpro26@gmail.com>
2026-01-28 07:56:00 -0300

    lista par impar

--- ExerciciosPython/exercicios/des084.py
+++ ExerciciosPython/exercicios/des084.py
@@ -3,7 +3,7 @@ dado = list()
 totpes = totlev = 0
 while True:
     dado.append(str(input('Nome: ')))
-    dado.append(int(input('Peso: ')))
+    dado.append(float(input('Peso: ')))
     pessoas.append(dado[:])
     dado.clear()
     resp = str(input('Quer continuar? [S/N]'))
@@ -17,6 +17,7 @@ for p in pessoas:
     else:
         print(f'{p[0]} com o pesso {p[1]} está com o peso normal')
         totlev += 1
+print(f'Você cadastrou {len(pessoas)} pessoas')
 print(f'Tem {totpes} pessoas acima do peso e {totlev} pesspas com o peso normal ')
 
 
---
+++ ExerciciosPython/exercicios/des085.py
@@ -0,0 +1,13 @@
+num = [[], []]
+valor = list()
+for c in range(1, 8):
+    valor = int(input(f'Digite o {c}ª valor: '))
+    if valor % 2 == 0:
+        num[0].append(valor)
+    else:
+        num[1].append(valor)
+print('='*30)
+num[0].sort()
+num[1].sort()
+print(f'Os numeros pares digitados foram {num[0]}')
+print(f'Os numeros impares digitados foram {num[1]}')
