5d814134df0462eeb7263364b5174f12a47ec42e
Felipe André <felipe.tecpro26@gmail.com>
2026-02-03 11:22:55 -0300

    dicionario pc

---
+++ ExerciciosPython/exercicios/des090.py
@@ -0,0 +1,12 @@
+aluno = {}
+aluno['nome'] = str(input('Nome: '))
+aluno['media'] = float(input(f'Media de {aluno["nome"]}: '))
+if aluno['media'] >= 7:
+    aluno['situacao'] = 'Aprovado!'
+elif aluno['media'] < 5:
+    aluno['situacao'] = 'Reprovado!'
+else:
+    aluno['situacao'] = 'Recuperação!'
+print('-='*20)
+for k, v in aluno.items():
+    print(f'  - {k} é {v}')
