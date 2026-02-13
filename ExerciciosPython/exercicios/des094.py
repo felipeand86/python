998e8528d134aa2cde0fb61224c668ce63d0dd29
Felipe André <felipe.tecpro26@gmail.com>
2026-02-13 09:54:05 -0300

    uniao dict e list

---
+++ ExerciciosPython/exercicios/des094.py
@@ -0,0 +1,39 @@
+pessoa = dict()
+galera = list()
+soma = media = 0
+while True:
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
+    while True:
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO!! Digite apenas M ou F.')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO!! Digite apenas S ou N.')
+    if resp == 'N':
+        break
+print('-='*30)
+print(f'A) Ao todo temos {len(galera)} pessoas cadastradas.')
+media = soma / len(galera)
+print(f'B) A média de idade é de {media:5.2f} anos')
+print(f'C) As Mulheres cadastradas foram ', end='')
+for p in galera:
+    if p['sexo'] == 'F':
+        print(f'{p["nome"]} ', end='')
+print()
+print(f'D) Lista de pessoas que estão acima da media: ')
+for p in galera:
+    if p['idade'] >= media:
+        print('   ', end='')
+        for k, v in p.items():
+            print(f'{k} = {v}; ',end='')
+        print()
+print('-='*30)
+print('  << Encerrando >>  ')
